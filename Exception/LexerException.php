51a9ec53925605ddd1a6bb2b2b426085e2fb8773
knesmeyanov <k.nesmeyanov@rambler-co.ru>
2020-03-05 17:55:41 +0300

    Fix branch

--- Exception/LexerException.php
+++ Exception/LexerException.php
@@ -1,20 +1,19 @@
 <?php
-
 /**
- * This file is part of phplrt package.
+ * This file is part of Phplrt package.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 declare(strict_types=1);
 
 namespace Phplrt\Lexer\Exception;
 
-/**
- * Interface LexerExceptionInterface
- */
-interface LexerExceptionInterface extends \Throwable
-{
+use Phplrt\Exception\ExternalException;
 
+/**
+ * Class LexerException
+ */
+abstract class LexerException extends ExternalException
+{
 }
