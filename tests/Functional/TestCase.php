b0d4421c7e64ea2425bb3a73fb4558e6c3413770
SerafimArts <nesk@xakep.ru>
2024-07-28 13:23:41 +0000

    Add PHP 8.4 tests support

--- tests/Functional/TestCase.php
+++ tests/Functional/TestCase.php
@@ -11,6 +11,7 @@ abstract class TestCase extends BaseTestCase
 {
     /**
      * @param iterable|TokenInterface[] $result
+     *
      * @return array|TokenInterface[]
      */
     protected function tokensOf(iterable $result): array
